08ab77b02b2026c1c4d9833eadc927466bf46d0a
Juan David Patarroyo Sánchez <juandavid.patarroyosanchez@gmail.com>
2023-10-01 19:11:30 -0500

    UserInterface Clase FrMain y Login - CerrarSesion

--- UserInterface/FrLogin.py
+++ UserInterface/FrLogin.py
@@ -1,19 +1,18 @@
+import sys
+
+sys.path.append("Models")
 from ttkbootstrap.dialogs import Messagebox, MessageDialog
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from MdUsuario import MdUsuario
 from MdInicioSesion import MdInicioSesion
 from EnEnum import EnTipoUsuario
-import sys
-from tkinter import StringVar
-sys.path.append("Models")
 
-
-def FrLogin(root: ttk.Window, onCallBack) -> ttk.Toplevel:
+def FrLogin(objMain) -> ttk.Toplevel:
 
     # region Eventos
 
-    def btnInicio_onClick():
+    def onBtnInicio_onClick():
         mdInicioSesion = MdInicioSesion()
         mdInicioSesion.Usuario = txtUsuario.get()
         mdInicioSesion.Password = txtPassword.get()
@@ -25,18 +24,23 @@ def FrLogin(root: ttk.Window, onCallBack) -> ttk.Toplevel:
             case 'Administrador':
                 mdInicioSesion.TipoUsuario = EnTipoUsuario.Administrador
         if MdUsuario.ValidarUsuario(mdInicioSesion):
-            print('Usuario Autenticado')
             mdInicioSesion.IsLogin = True
             frLogin.destroy()
-            onCallBack(mdInicioSesion)
+            objMain.onAfterLogin(mdInicioSesion)
         else:
-            Messagebox.show_warning(
-                'Usuario o Constraseña invalida!!', parent=frLogin)
-
+            warning = Messagebox.show_warning('Usuario o Constraseña invalida!!', parent=frLogin, title='UNAL')
     # endregion
-    frLogin = ttk.Toplevel(root, topmost=True, resizable=(False, False))
+    #region Funciones
+    def CentrarPantalla(parentWidth, parentHeight, widgetWidht, widgetHeight):
+        x = parentWidth//2 - widgetWidht//2
+        y = parentHeight//2 - widgetHeight//2
+        return f'{widgetWidht}x{widgetHeight}+{x}+{y}'
+    #endregion
+
+    frLogin = ttk.Toplevel(objMain.root, topmost=True, resizable=(False, False))
     frLogin.title('UNAL - Control de Asistencia - Inicio Sesión')
-    frLogin.geometry('480x350')
+    geometryLogin = CentrarPantalla(objMain.root.winfo_screenwidth(), objMain.root.winfo_screenheight(), 480, 350)
+    frLogin.geometry(geometryLogin)
     frLogin.columnconfigure(0, weight=1)
     frLogin.rowconfigure(0, weight=1)
 
@@ -44,31 +48,31 @@ def FrLogin(root: ttk.Window, onCallBack) -> ttk.Toplevel:
     frContent.grid(column=0, row=0, padx=70, sticky=(N, W, E, S))
 
     lbTitulo = ttk.Label(frContent, text='Inicio de Sesión',
-                         font=('Helvetica', 24), bootstyle="success")
+                         font=('Helvetica', 24), bootstyle="success",)
     lbTitulo.grid(column=0, row=0, pady=50, columnspan=2)
 
     lbTipoCuenta = ttk.Label(
-        frContent, text='Tipo de cuenta:', bootstyle="success", width=20)
+        frContent, text='Tipo de cuenta:', bootstyle="success", width=20, anchor='e')
     lbTipoCuenta.grid(column=0, row=1, pady=5)
 
-    TiposUsuario = ['Administrador', 'Docente', 'Estudiante']
+    TiposUsuario = ['Docente', 'Estudiante','Administrador']
     cbTipoUsuario = ttk.Combobox(
-        frContent, style=SUCCESS, state="readonly", values=TiposUsuario, width=30)
+        frContent, style=SUCCESS, state="readonly", values=TiposUsuario, width=30,)
     cbTipoUsuario.grid(column=1, row=1, pady=5)
     cbTipoUsuario.current(0)
 
-    lbUsuario = ttk.Label(frContent, text='Usuario:', bootstyle="success", width=20)
+    lbUsuario = ttk.Label(frContent, text='Usuario:', bootstyle="success", width=20, anchor='e')
     lbUsuario.grid(column=0, row=2, pady=5)
     txtUsuario = ttk.Entry(frContent, bootstyle="success", width=32)
     txtUsuario.grid(column=1, row=2, pady=5)
 
-    lbPassword = ttk.Label(frContent, text='Password:', bootstyle="success", width=20)
+    lbPassword = ttk.Label(frContent, text='Password:', bootstyle="success", width=20, anchor='e')
     lbPassword.grid(column=0, row=3, pady=5)
     txtPassword = ttk.Entry(frContent, show='*', bootstyle="success", width=32)
     txtPassword.grid(column=1, row=3, pady=5)
 
     btnInicio = ttk.Button(frContent, text='Iniciar sesión',
-                           command=btnInicio_onClick,  bootstyle='success', width=30)
+                           command=onBtnInicio_onClick,  bootstyle='success', width=30)
     btnInicio.grid(column=0, row=4, pady=30, columnspan=2)
 
     frLogin.wait_window()
--- UserInterface/FrMain.py
+++ UserInterface/FrMain.py
@@ -1,40 +1,99 @@
 import sys
 sys.path.append("Models")
-
-import ttkbootstrap as ttk
-from ttkbootstrap.constants import *
-from MdInicioSesion import MdInicioSesion
-from FrLogin import FrLogin
 from EnEnum import EnTipoUsuario
+from FrLogin import FrLogin
+from MdInicioSesion import MdInicioSesion
+from ttkbootstrap.constants import *
+import ttkbootstrap as ttk
 
-#region Eventos
+class FrMain():
 
-def onAfterLogin(mdInicioSesion: MdInicioSesion):
-    global objInicioSesion
-    objInicioSesion = mdInicioSesion
-    ConfigurarMenu()
+    # region Propiedades
 
-def ConfigurarMenu():
-    match objInicioSesion.TipoUsuario:
-        case EnTipoUsuario.Administrador:
-            btnDocentes = ttk.Button(root, text='Administrar docentes', bootstyle='success').grid(row=0, column=0, sticky="ew")
-            ttk.Frame.columnconfigure(0, weight=1)
-            btnEstudiantes = ttk.Button(root, text='Administrar estudiantes', bootstyle='success').grid(row=0, column=1)
-            btnAsignaturas = ttk.Button(root, text='Administrar asignaturas', bootstyle='success').grid(row=1, column=0)
-        case EnTipoUsuario.Estudiante:
-            btnAsistencia = ttk.Button(root, text='Consultar asistencia', bootstyle='success').grid(row=5)
-        case EnTipoUsuario.Docente:
-            btnClase = ttk.Button(root, text='Iniciar clase', bootstyle='success').grid(row=0, column=0)
-            btnAñadir = ttk.Button(root, text='Añadir estudiante', bootstyle='success').grid(row=0, column=4)
-#endregion
+    root: ttk.Window
+    frameHeader: ttk.Frame
+    frameInfo: ttk.Frame
+    objInicioSesion: MdInicioSesion
 
-#region Formulario
+    #endregion
 
-root = ttk.Window(themename='flatly')
-root.title('UNAL - Control de Asistencia')
-root.state('zoomed')
-FrLogin(root, onAfterLogin)
+    # region Constructores
 
-root.mainloop()
+    def __init__(self) -> None:
+        self.root = ttk.Window(themename='flatly')
+        self.root.title('UNAL - Control de Asistencia')
+        self.root.state('zoomed')
+        self.MostrarHeader()
+        FrLogin(self)
+        self.root.mainloop()
 
-#endregion 
+    # endregion
+
+    # region Funciones
+
+    def MostrarHeader(self):
+        self.frameHeader = ttk.Frame(self.root)
+        self.frameHeader.grid(row=0, column=0)
+        logo = ttk.PhotoImage(file="UserInterface\Images\Logo.png")
+        labelLogo = ttk.Label(self.frameHeader, image=logo, width=30)
+        labelLogo.grid(row=0, column=0)
+        title = ttk.Label(self.frameHeader, text="UNAL - Sistema de asistencia",
+                        bootstyle='success', font=('Helvetica', 30), width=40)
+        title.grid(row=0, column=1)
+
+    def MostrarInfo(self):
+        self.frameInfo = ttk.Frame(self.frameHeader, width=50)
+        self.frameInfo.grid(row=0, column=2)
+        labelUsuario = ttk.Label(
+            self.frameInfo, bootstyle='success', text=self.objInicioSesion.Usuario, font=('Helvetica', 12))
+        labelUsuario.grid(row=0, column=0)
+        labelTipoUsuario = ttk.Label(
+            self.frameInfo, bootstyle='success', text=self.objInicioSesion.TipoUsuario.name, font=('Helvetica', 8))
+        labelTipoUsuario.grid(row=1, column=0)
+        btnCerrarSesion = ttk.Button(
+            self.frameInfo, text='Cerrar Sesión', bootstyle='success', command=self.onBtnCerrarSesion_onClick)
+        btnCerrarSesion.grid(row=12, column=0)
+
+    def ConfigurarMenu(self):
+        frameButtons = ttk.Frame(self.root)
+        frameButtons.grid(row=2, column=0)
+        match self.objInicioSesion.TipoUsuario:
+            case EnTipoUsuario.Administrador:
+                btnDocentes = ttk.Button(
+                    frameButtons, text='Administrar docentes', bootstyle='success')
+                btnDocentes.grid(column=0, row=0, padx=5)
+                btnEstudiantes = ttk.Button(
+                    frameButtons, text='Administrar estudiantes', bootstyle='success')
+                btnEstudiantes.grid(column=1, row=0, padx=5)
+                btnAsignaturas = ttk.Button(
+                    frameButtons, text='Administrar asignaturas', bootstyle='success')
+                btnAsignaturas.grid(column=2, row=0, padx=5)
+            case EnTipoUsuario.Estudiante:
+                btnAsistencia = ttk.Button(
+                    frameButtons, text='Consultar asistencia', bootstyle='success')
+                btnAsistencia.grid(column=0, row=0, padx=5)
+            case EnTipoUsuario.Docente:
+                btnClase = ttk.Button(
+                    frameButtons, text='Iniciar clase', bootstyle='success')
+                btnClase.grid(column=0, row=0, padx=5)
+                btnAñadir = ttk.Button(
+                    frameButtons, text='Añadir estudiante', bootstyle='success')
+                btnAñadir.grid(column=0, row=0, padx=5)
+
+    def onAfterLogin(self, mdInicioSesion: MdInicioSesion):
+        self.objInicioSesion = mdInicioSesion
+        self.MostrarInfo()
+        self.ConfigurarMenu()
+
+    #endregion
+
+    # region Eventos
+  
+    def onBtnCerrarSesion_onClick(self):
+        self.frameInfo.destroy()
+        self.objInicioSesion = None
+        FrLogin(self)
+
+    # endregion
+
+objInit = FrMain()
